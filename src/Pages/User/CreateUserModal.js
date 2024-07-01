8b02462efa7f622b47da8b351fa312b07606b8ab
Kishore Chinnaswamy <kishore25chinnaswamy@gmail.com>
2024-07-01 21:26:51 +0530

    Updated User Modal

--- src/Pages/User/CreateUserModal.js
+++ src/Pages/User/CreateUserModal.js
@@ -1,9 +1,45 @@
 import React, { useState, useEffect } from "react";
-import { Modal, Button, Form } from "react-bootstrap";
 import axios from "axios";
-import { Alert, Snackbar } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  Grid,
+  InputLabel,
+  Modal,
+  Snackbar,
+  Typography,
+} from "@mui/material";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+
+import { DatePicker } from "rsuite";
+import { TextField } from "../../components/textField/TextField";
+import { Button } from "react-bootstrap";
+
+const options = {
+  month: "short",
+  day: "numeric",
+  year: "numeric",
+  timeZone: "GMT",
+};
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 800,
+  bgcolor: "background.paper",
+  // border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  borderRadius: 2,
+  height: 500,
+  overflow: "auto",
+};
 
 const userSchema = Yup.object().shape({
   username: Yup.string().required("Username is required."),
@@ -13,83 +49,46 @@ const userSchema = Yup.object().shape({
     .min("10", "Please enter a valid mobile number.")
     .max("10", "Please enter a valid mobile number.")
     .required("Mobile number is required."),
-  dateofbirth: Yup.date().required("Date of Birth is required"),
+  date_of_birth: Yup.date().required("Date of Birth is required"),
   address: Yup.string().required("Address is required."),
 });
 
-const CreateUserModal = ({
+export default function CreateUserModal({
   handleCloseAddUserModal,
   fetchData,
   isUserEdit,
   currentUser,
   handleUserEdit,
-}) => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [mobilenumber, setMobilenumber] = useState("");
-  const [is_admin, setIsAdmin] = useState(false);
-  const [address, setAddress] = useState("");
-  const [date_of_birth, setDateOfBirth] = useState("");
-  const [marriage_date, setMarriageDate] = useState("");
-
+}) {
   const [notification, setNotification] = useState(null);
   const [openNotification, setOpenNotification] = useState(false);
   const [notificationSeverity, setNotificationSeverity] = useState("");
 
-  useEffect(() => {
-    if (isUserEdit && currentUser) {
-      setUsername(currentUser.username);
-      setEmail(currentUser.email);
-      setMobilenumber(currentUser.mobilenumber);
-      setIsAdmin(currentUser.is_admin);
-      setAddress(currentUser.address);
-      setDateOfBirth(currentUser.date_of_birth);
-      setMarriageDate(currentUser.marriage_date);
-    } else {
-      setUsername("");
-      setEmail("");
-      setPassword("");
-      setMobilenumber("");
-      setIsAdmin(false);
-      setAddress("");
-      setDateOfBirth("");
-      setMarriageDate("");
-    }
-  }, [isUserEdit, currentUser]);
+  let initialValues = {};
+  if (isUserEdit) {
+    initialValues = {
+      ...currentUser,
+      marriage_date: new Date(currentUser?.marriage_date),
+      date_of_birth: new Date(currentUser?.date_of_birth),
+    };
+  }
 
-  useEffect(() => {
-    if (currentUser?.date_of_birth) {
-      const formattedDate = new Date(currentUser.date_of_birth)
-        .toISOString()
-        .split("T")[0];
-      setDateOfBirth(formattedDate);
-    }
-    if (currentUser?.marriage_date) {
-      const formattedDate = new Date(currentUser.marriage_date)
-        .toISOString()
-        .split("T")[0];
-      setMarriageDate(formattedDate);
-    }
-  }, [currentUser]);
+  console.log("currentUser", currentUser);
 
-  const handleSave = async () => {
-    try {
-      const user = {
-        username,
-        email,
-        password,
-        mobilenumber,
-        is_admin,
-        address,
-        date_of_birth,
-        marriage_date,
-      };
+  const formik = useFormik({
+    initialValues: { ...initialValues },
+    validationSchema: userSchema,
+    validateOnChange: false,
+    enableReinitialize: true,
+    onSubmit: (values) => {
+      console.log("add user", values);
+      values.is_admin = values.is_admin ? values.is_admin : false;
       if (isUserEdit) {
+        console.log("edit");
         axios
           .put(
             `http://localhost:3000/api/v1/user/updateUserById/?id=${currentUser?.user_id}`,
-            user
+            values
           )
           .then(() => {
             if (isUserEdit) {
@@ -110,7 +109,7 @@ const CreateUserModal = ({
           });
       } else {
         axios
-          .post("http://localhost:3000/api/v1/user/createUser", user)
+          .post("http://localhost:3000/api/v1/user/createUser", values)
           .then(() => {
             if (isUserEdit) {
               handleUserEdit();
@@ -129,10 +128,8 @@ const CreateUserModal = ({
             setNotificationSeverity("error");
           });
       }
-    } catch (error) {
-      console.error("Error saving user:", error);
-    }
-  };
+    },
+  });
 
   return (
     <>
@@ -154,105 +151,242 @@ const CreateUserModal = ({
           {notification}
         </Alert>
       </Snackbar>
-      <Modal show={true} onHide={handleCloseAddUserModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>{isUserEdit ? "Edit User" : "Create User"}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group controlId="formUsername">
-              <Form.Label>Username</Form.Label>
-              <Form.Control
-                type="text"
+      <Modal
+        open={true}
+        onClose={() => {
+          //   formik.resetForm();
+          handleCloseAddUserModal();
+        }}
+      >
+        <Box sx={style}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h5" sx={{ fontWeight: "700" }}>
+                {isUserEdit ? "Edit" : "Add"} User
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                label="Username"
+                placeholder="Username"
+                name="username"
                 required
-                placeholder="Enter username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
+                id="user-username"
+                containerClass="user-field"
+                value={formik.values.username}
+                onChange={formik.handleChange}
               />
-            </Form.Group>
-            <Form.Group controlId="formEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
+              {formik.errors.username ? (
+                <InputLabel
+                  // className={classes.error}
+                  sx={{ color: "red !important" }}
+                >
+                  {formik.errors.username}
+                </InputLabel>
+              ) : null}
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                label="Email"
+                placeholder="Email"
+                name="email"
+                id="user-email"
+                containerClass="user-field"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+              />
+              {formik.errors.email ? (
+                <InputLabel
+                  // className={classes.error}
+                  sx={{ color: "red !important" }}
+                >
+                  {formik.errors.email}
+                </InputLabel>
+              ) : null}
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                label="Password"
+                placeholder="Password"
+                name="password"
                 required
-                placeholder="Enter email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                id="user-password"
+                containerClass="user-field"
+                value={formik.values.password}
+                onChange={formik.handleChange}
               />
-            </Form.Group>
-            {!isUserEdit && (
-              <Form.Group controlId="formPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  required
-                  placeholder="Enter password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
+              {formik.errors.password ? (
+                <InputLabel
+                  // className={classes.error}
+                  sx={{ color: "red !important" }}
+                >
+                  {formik.errors.password}
+                </InputLabel>
+              ) : null}
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                label="Mobile Number"
+                placeholder="Mobile Number"
+                required
+                id="customer-mobile-number"
+                containerClass="customer-field"
+                name="mobilenumber"
+                value={formik.values.mobilenumber}
+                onChange={formik.handleChange}
+                type="number"
+              />
+              {formik.errors.mobilenumber ? (
+                <InputLabel sx={{ color: "red !important" }}>
+                  {formik.errors.mobilenumber}
+                </InputLabel>
+              ) : null}
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <InputLabel className="customer-field-label">
+                Date of Birth{" "}
+                <span style={{ color: "red", marginLeft: "2px" }}>*</span>
+              </InputLabel>
+
+              <DatePicker
+                placement="topStart"
+                style={{ width: "94%" }}
+                // className="customer-field"
+                character="-"
+                format="dd-MM-yyyy"
+                size="md"
+                placeholder="Date of Birth"
+                onChange={(e) => {
+                  console.log("e", e);
+                  formik.setFieldValue("date_of_birth", e);
+                }}
+                // onChange={(date) => setDate(date)}
+                // value={date}
+                renderValue={(date) => {
+                  return `${new Date(
+                    formik.values.date_of_birth
+                  ).toLocaleDateString("en-EN", options)}`;
+                }}
+                name="date"
+                value={formik.values.date_of_birth}
+              />
+              {formik.errors.date_of_birth ? (
+                <InputLabel
+                  // className={classes.error}
+                  sx={{ color: "red !important" }}
+                >
+                  {formik.errors.date_of_birth}
+                </InputLabel>
+              ) : null}
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <InputLabel className="customer-field-label">
+                Address{" "}
+                <span style={{ color: "red", marginLeft: "2px" }}>*</span>
+              </InputLabel>
+
+              <textarea
+                placeholder="Address"
+                rows="3"
+                style={{ width: "94%", marginTop: "1%" }}
+                name="address"
+                onChange={formik.handleChange}
+                value={formik.values.address}
+              />
+              {formik.errors.address ? (
+                <InputLabel
+                  // className={classes.error}
+                  sx={{ color: "red !important" }}
+                >
+                  {formik.errors.address}
+                </InputLabel>
+              ) : null}
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <InputLabel className="customer-field-label">
+                Marriage Date
+              </InputLabel>
+
+              <DatePicker
+                placement="topStart"
+                style={{ width: "94%" }}
+                // className="customer-field"
+                character="-"
+                format="dd-MM-yyyy"
+                size="md"
+                placeholder="Marriage Date"
+                onChange={(e) => {
+                  formik.setFieldValue("marriage_date", e);
+                }}
+                // value={date}
+                renderValue={(date) => {
+                  return `${new Date(
+                    formik.values.marriage_date
+                  ).toLocaleDateString("en-EN", options)}`;
+                }}
+                name="date"
+                value={formik.values.marriage_date}
+              />
+              {formik.errors.marriage_date ? (
+                <InputLabel
+                  // className={classes.error}
+                  sx={{ color: "red !important" }}
+                >
+                  {formik.errors.marriage_date}
+                </InputLabel>
+              ) : null}
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <FormGroup>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      onChange={(e) => {
+                        formik.setFieldValue("is_admin", e.target.checked);
+                      }}
+                    />
+                  }
+                  label="Is Admin?"
                 />
-              </Form.Group>
-            )}
-            <Form.Group controlId="formIsAdmin">
-              <Form.Check
-                type="checkbox"
-                label="Admin"
-                checked={is_admin}
-                onChange={(e) => setIsAdmin(e.target.checked)}
-              />
-            </Form.Group>
-            <Form.Group controlId="formDateOfBirth">
-              <Form.Label>Date of Birth</Form.Label>
-              <Form.Control
-                type="date"
-                required
-                placeholder="Enter Date of Birth"
-                value={date_of_birth}
-                onChange={(e) => setDateOfBirth(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="formMarriageDate">
-              <Form.Label>Marriage Date</Form.Label>
-              <Form.Control
-                type="date"
-                required
-                placeholder="Enter Marriage Date"
-                value={marriage_date}
-                onChange={(e) => setMarriageDate(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="formAddress">
-              <Form.Label>Address</Form.Label>
-              <Form.Control
-                type="text"
-                required
-                placeholder="Enter Address"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="formMobilenumber">
-              <Form.Label>Mobile Number</Form.Label>
-              <Form.Control
-                type="text"
-                required
-                placeholder="Enter mobile number"
-                value={mobilenumber}
-                onChange={(e) => setMobilenumber(e.target.value)}
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseAddUserModal}>
-            Cancel
-          </Button>
-          <Button variant="primary" type="submit" onClick={() => handleSave()}>
-            Submit
-          </Button>
-        </Modal.Footer>
+              </FormGroup>
+            </Grid>
+            <Grid item xs={12} container>
+              <Grid item xs={12} md={10} textAlign="end">
+                <Button
+                  variant="secondary"
+                  onClick={() => {
+                    formik.resetForm();
+                    handleCloseAddUserModal();
+                  }}
+                >
+                  Cancel
+                </Button>
+              </Grid>
+              <Grid item xs={12} md={2} textAlign="end">
+                <Button
+                  variant="primary"
+                  type="submit"
+                  onClick={() => {
+                    console.log("click");
+                    formik.handleSubmit();
+                  }}
+                >
+                  Add
+                </Button>
+                {/* <button
+                type="submit"
+                onClick={() => {
+                  console.log("click");
+                  formik.handleSubmit();
+                }}
+              >
+                Add
+              </button> */}
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
       </Modal>
     </>
   );
-};
-
-export default CreateUserModal;
+}
